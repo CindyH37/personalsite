80cc82fb02755e7c911a3026dae68e230cf21b1f
CindyH37 <66810033+CindyH37@users.noreply.github.com>
2022-10-09 21:53:30 -0400

    Home site big update
    Rewrote all main sections + added poem

--- pages/index.js
+++ pages/index.js
@@ -6,90 +6,75 @@ export default function Home() {
   return (
     <div className="leading-[2.0]">
       <div>
-        <p className="pt-10 text-center text-5xl">Salut, I'm Cindy!</p>
+        <p className="pt-10 text-center text-5xl">Hi, I'm Cindy.</p>
       </div>
       <br />
       <div className="flex gap-8">
         <div className="w-3/5">
-          I'm a rising freshman at{' '}
+          Welcome to my site! I'm a first-year student at{' '}
           <a href="https://www.williams.edu/" target="_blank" rel="noreferrer">
             Williams College
           </a>{' '}
-          and a cofounder of{' '}
+          interested in mathematics, economics, and comparative literature. (Yes, all at once.) I
+          also like looking at pineapples but not eating them.
+          {/* HYPERLINK CODE HERE: I'm also a cofounder of{' '}
           <a href="https://www.nexusworkshop.org" target="_blank" rel="noreferrer">
             The Nexus Workshop
           </a>
-          . I'm broadly interested in mathematics and exploring languages.
+          . */}
           {/* Currently Exploring */}
           <br /> <br />
-          <p className="text-center text-2xl font-bold">Currently Exploring</p>
+          <p className="text-center text-2xl font-bold">Currently Interested In</p>
           <ul>
             <li>
-              Wrapping up my{' '}
-              <a
-                href="https://en.wikipedia.org/wiki/Printmaking"
-                target="_blank"
-                rel="nonreferrer noreferrer"
-              >
-                printmaking
-              </a>{' '}
-              class and crocheting my second wool beanie because my first one looked like an ugly
-              candy corn :D
+              Effective Altruism: What does it really mean to do good and how can we maximize our
+              impact on the world?
             </li>
             <li>
-              Attending{' '}
-              <a href="https://www.startupschool.org/" target="_blank" rel="noreferrer">
-                Startup School
-              </a>
-              , possibly the most informative free online program I've ever participated in.
+              Language: How does it change the way we think and tell stories? Why does everything
+              get "lost in translation?" How might I go about becoming a polyglot?
             </li>
             <li>
-              Reading{' '}
-              <a
-                href="https://www.elizabethgilbert.com/books/eat-pray-love/"
-                target="_blank"
-                rel="nonreferrer noreferrer"
-              >
-                Eat, Pray, Love
-              </a>{' '}
-              by Elizabeth Gilbert. This is a memoir about - get this - eating, praying, and loving.
-              I needed something uplifting and so far it's been amazing.
+              Beauty in Academia: Is beauty reason enough to pursue a subject? What even is beauty,
+              anyway (especially contrasted with functionality)? Does all of mathematics have
+              real-world applications? And most importantly, does it need to in order to be
+              worthwhile?
             </li>
           </ul>
           {/* Up Next */}
           <br />
-          <p className="text-center text-2xl font-bold">Up Next</p>
+          <p className="text-center text-2xl font-bold">
+            Less Philosophical Things Keeping Me Busy
+          </p>
           <ul>
-            <li>Moving into college!</li>
             <li>
-              Reading{' '}
-              <a
-                href="http://www.emilymandel.com/sot.html"
-                target="_blank"
-                rel="nonreferrer noreferrer"
-              >
-                Sea of Tranquility
-              </a>{' '}
-              by Emily St. John Mandel.
+              My Classes: Probability, Advanced Chinese, The Nature of Narrative, Principles of
+              Microeconomics
             </li>
             <li>
-              Crocheting a cow keychain and an{' '}
-              <a
-                href="https://en.wikipedia.org/wiki/Amigurumi"
-                target="_blank"
-                rel="nonreferrer noreferrer"
-              >
-                amigurumi
-              </a>{' '}
-              bunny to keep me company at college.
+              Things I Sometimes Go To: Effective Altruism Discussions, Chinese and French Language
+              Tables, Mathematics Faculty Seminars
             </li>
+            <li>Trying to Learn: Flow Arts (Poi)</li>
           </ul>
           <br />
         </div>
         <div className="mx-auto block w-1/4">
           <Image src="/static/images/pineapple.png" width="550" height="600" />
-
           {/* Put your Dickinson poem here */}
+          <p className="font-style: italic">
+            I'm Nobody! Who are you? <br />
+            Are you – Nobody – too? <br />
+            Then there's a pair of us! <br />
+            Don't tell! they'd advertise – you know!
+            <br /> <br />
+            How dreary – to be – Somebody! <br />
+            How public – like a Frog – <br />
+            To tell one's name – the livelong June – <br />
+            To an admiring Bog!
+            <br /> <br />
+          </p>
+          - Dickinson
         </div>
       </div>
     </div>
